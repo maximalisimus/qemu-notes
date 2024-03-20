65d0d79286d74d861989bf99c3ec8880b94592ae
Mikl <maximalis171091@yandex.ru>
2024-03-20 21:37:16 +0300

    Update

--- qemu-bat-sh/qemu.sh
+++ qemu-bat-sh/qemu.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# qemu-img create -f qcow2 test.qcow 30G
+# qemu-img create -f qcow2 hard.qcow 30G
 # -o subformat=fixed
 # -o subformat=dynamic
+#
+# qemu-img resize disk.qcow2 +10G
+# Please resize into disk.qcow2 and before command:
+# qemu-img resize --shrink disk.qcow2 -10G
+#
+# qemu-system-x86_64 -device help > device.txt
+# qemu-system-x86_64 -cpu help > cpu.txt
+#
 
 qemu-system-x86_64 \
 -enable-kvm \
 -cpu host \
 -smp cores=1 \
--m 1024 \
+-m 2048 \
 -machine q35 \
 -device intel-iommu \
 -vga virtio \
 -device e1000,netdev=wan \
 -netdev user,id=wan \
 -boot menu=on \
+-hda hard.qcow
+#-cdrom ./Disk-Image.iso \
+#-drive format=raw,file=raw.img
 # -snapshot \
 
 #-L . \
@@ -23,7 +34,28 @@ qemu-system-x86_64 \
 # или
 #-drive file=OVMF_CODE.fd,if=pflash,format=raw,unit=0,readonly=on
 
+#
 #-hda hard.qcow
+#
+#-fda file
+#-fdb file
+# or
+# -drive file=file,index=0,if=floppy
+# -drive file=file,index=1,if=floppy
+#
+#-hda file
+#-hdb file
+#-hdc file
+#-hdd file
+#
+# or
+# qemu -drive file=file,index=0,media=disk
+# qemu -drive file=file,index=1,media=disk
+# qemu -drive file=file,index=2,media=disk
+# qemu -drive file=file,index=3,media=disk
+#
+
+
 
 #-hda /dev/sdX
 #-hda ubuntu.qcow
@@ -33,10 +65,40 @@ qemu-system-x86_64 \
 #-drive format=vpc,file=image_vhd.vhd
 #-drive format=vdi,file=image_vdi.vdi
 
-# -usb --device usb-host,vendorid=0x125f,productid=0xdb8a
-
+#
+# -cdrom
+# or
+# -drive file=file,index=2,media=cdrom
+#
+# CDROM slave ide0
+# -drive file=file,if=ide,index=1,media=cdrom
+#
+# Empty CDROM
+# -drive if=ide,index=1,media=cdrom
+#
+# SCSI ID 6 buss 0
+# -drive file=file,if=scsi,bus=0,unit=6
+#
 # Windows 
 # -hda \\.\PhysicalDrive1
+#
+
+
+
+#
+# -usb --device usb-host,vendorid=0x125f,productid=0xdb8a
+#
+
+#
+# -smb dir
+# 10.0.2.4 smbserver
+# add to C:\WINDOWS\LMHOSTS on Windows 9x/Me or C:\WINNT\SYSTEM32\DRIVERS\ETC\LMHOSTS on Windows NT/2000.
+# \\smbserver\qemu
+#
+# HOST
+# /usr/sbin/smbd
+# 
+
 
 # -device e1000,netdev=wan \
 # -netdev user,id=wan,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:80,hostfwd=tcp::4443-:443 \
@@ -70,15 +132,127 @@ qemu-system-x86_64 \
 # $ sudo ip link delete br0 type bridge
 
 
-# dd if=/dev/zero of=hard.img bs=1M count=1024
-# mkfs -t ext4 hard.img
-# mkdir -p ./raw
-# sudo mount -t auto -o loop hard.img ./raw
-# sudo chown mikl:users ./raw
-# sudo chmod 777 ./raw
-# sudo umount -l ./raw
-# sudo rm -rf ./raw
-# sudo mkdir -p ./disk
-# sudo mount -t auto -o loop hard.img ./disk
-
+# dd if=/dev/zero of=raw.img bs=1M count=1024
+# mkfs -t ext4 raw.img
+# mkdir -p ./disk
+# sudo mount -t auto -o loop raw.img ./disk
+# sudo chown -R mikl:users ./disk
+# sudo chmod -R 777 ./disk
+# sudo chown root:root ./disk/lost+found
+# sudo chmod 0700 ./disk/lost+found
 # sudo umount -l ./disk
+
+
+
+# mkdir -p script && 7z x image.iso -o./script/ && rm -rf && ./script/[SYS] ./script/lost+found && tar -czf archive.tar.gz script/ && rm -rf ./script
+# mkdir -p temp && mv image.iso ./temp/ && cd ./temp && 7z x image.iso && 7z a '-x!image.iso' '-x![SYS]' '-x!lost+found' -t7z -mx7 ../archive.7z && mv image.iso ../ && cd ../ && rm -rf ./temp/
+# 7z a '-x!disk/[SYS]' '-x!disk/lost+found' -t7z -mx7 archive.7z ./disk
+#  cd ./disk && 7z a '-x![SYS]' '-x!lost+found' -t7z -mx7 ../archive.7z && cd ../
+
+
+
+
+#
+# dd if=/dev/zero of=raw.img bs=4096k count=640
+# parted -s raw.img print
+#
+# parted -s raw.img mklabel msdos
+# or
+# parted -s raw.img mklabel gpt
+#
+# parted -s raw.img mkpart primary ntfs 1MiB 100% 
+#
+# EFI Part:
+# parted -s DEVICE mkpart ESP fat32 1MiB 513MiB
+# parted -s DEVICE set 1 boot on
+# parted -s raw.img print
+#
+
+
+
+#
+# sudo pacman -S nbd ntfs-3g dosfstools --noconfirm
+# sudo modprobe nbd
+#
+# sudo qemu-nbd -c /dev/nbd0 ./raw.img
+#
+# VDI, VHD, VMDK, VHDX, RAW
+# cfdisk, cgdisk, gparted, parted
+#
+# lsblk -o NAME,MODEL,TYPE,FSTYPE,SIZE,MOUNTPOINT /dev/nbd0
+# mkfs -t ntfs /dev/nbd0p1
+#
+# sudo qemu-nbd -d /dev/nbd0
+#
+# parted -s raw.img print
+#
+# raw(img) vpc(vhd) vdi(vdi) vhdx(vhdx) qcow2(qcow)
+# qemu-img convert -p -f raw -O vdi raw.img raw_vdi.vdi
+#
+# qemu-img convert -p -f raw -O vpc raw.img raw_vhd.vhd
+#
+# Example:
+# qemu-img create -f vpc -o size=4G ./image_vhd.vhd
+# sudo qemu-nbd -c /dev/nbd0 ./image_vhd.vhd
+# parted -s /dev/nbd0 mklabel msdos
+# parted -s /dev/nbd0 mkpart primary ntfs 1MB 2561MB
+# parted -s /dev/nbd0 mpart primary ext4 2562MB 100%
+# sudo qemu-nbd -d /dev/nbd0
+# sudo qemu-nbd -c /dev/nbd0 ./image_vhd.vhd
+# mkfs -t ntfs /dev/nbd0p1
+# mkfs -t ext4 /dev/nbd0p2
+# sudo qemu-nbd -d /dev/nbd0
+# sudo qemu-nbd -c /dev/nbd0 ./image_vhd.vhd
+# lsblk -o NAME,MODEL,TYPE,FSTYPE,SIZE,MOUNTPOINT /dev/nbd0
+# parted -s /dev/nbd0 print
+# id -g `$whoami`
+# id -u `$whoami`
+# sudo mount -t auto -o,rw,gid=985,uid=1000 /dev/nbd0p1 ./disk
+# sudo umount -l ./disk
+# sudo qemu-nbd -d /dev/nbd0
+#
+
+
+
+
+
+# Create Snapshot
+# qemu-img snapshot -c snapshotName vmpath/disk.qcow2
+
+# List Snapshot
+# qemu-img snapshot -l vmpath/disk.qcow2
+
+# Restore Snapshot
+# qemu-img snapshot -a snapshotName vmpath/disk.qcow2
+
+# Delete Snapshot
+# qemu-img snapshot -d snapshotName vmpath/disk.qcow2
+
+# Variant 2.
+# qemu-img create -f qcow2 -F qcow2 -b vmpath/disk.qcow2 diffDisk.qcow2
+# qemu-system-x86_64 -m 1G -drive file=vmpath/diffDisk.qcow2,media=disk
+# qemu-system-x86_64 -m 1G -drive file=vmpath/disk.qcow2 ,media=disk
+# rm vmpath/diffDisk.qcow2
+
+
+
+
+
+
+
+# -netdev user,id=lan,restrict=off: создает виртуальный сетевой интерфейс с именем lan и типом user, который позволяет виртуальной машине общаться с внешним миром через сетевой стек хоста. Опция restrict=off означает, что нет ограничений на подключения входящих и исходящих соединений;
+# -netdev socket,id=sock,connect=<IP>:443: создает сетевой интерфейс типа socket с именем sock, который позволяет подключиться к удаленному серверу по указанному IP-адресу и порту 443;
+# -netdev hubport,id=port-lan,hubid=0,netdev=lan: добавляет порт к виртуальному хабу (с hubid=0), который связан с виртуальным сетевым интерфейсом lan;
+# -netdev hubport,id=port-sock,hubid=0,netdev=sock: аналогично предыдущему добавляет еще один порт к тому же виртуальному хабу, связанный с виртуальным сетевым интерфейсом sock;
+# -nographic: запускает QEMU без графического интерфейса, используя консоль для вывода.
+
+
+
+
+
+
+
+
+
+
+
